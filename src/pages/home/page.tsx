4e6b9fafde221c3a2959dd10262e09b17255c6a3
Min-gyu <jorushs@gmail.com>
2025-02-25 16:32:00 +0900

    [Init] 초기 프로젝트 세팅 - pakage.json 세팅 - 기타 config 세팅

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">여기 저기~</h1>
+    </div>
+  );
+}
