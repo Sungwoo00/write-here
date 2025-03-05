fb02169b7c4365746dceb1f877988fff25eff6df
BomEllen <jjjj200100@gmail.com>
2025-03-05 14:15:30 +0900

    [Feat] SubmitButton 컴포넌트 제작
    - 로그인에서 사용되는 SubmitButton 컴포넌트를 제작하였습니다.
    
    resolved #13

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,24 @@
+import '../styles/global.css';
+
+function SubmitButton({ disable = true }) {
+  return (
+    <button
+      type="submit"
+      className={`
+        w-full 
+        h-9 
+        text-white 
+        font-[HSSanTokki] 
+        rounded-md 
+        ${!disable ? 'bg-[var(--logo-green)]' : 'bg-[var(--light-gray)]'} 
+        ${!disable ? 'cursor-pointer' : 'cursor-not-allowed'}
+        ${!disable ? 'hover:opacity-80' : ''}
+      `}
+      disabled={disable}
+    >
+      로그인
+    </button>
+  );
+}
+
+export default SubmitButton;
