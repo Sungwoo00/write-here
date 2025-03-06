0535653522ea44b5808f749d32f908ebe469da6f
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-03-06 17:48:54 +0900

    [Modify] Spring 컴포넌트
    - svg파일 절대 경로로 변경

---
+++ src/components/Spring.tsx
@@ -0,0 +1,13 @@
+function Spring() {
+  return (
+    <img
+      src="/icons/spring.svg"
+      alt=""
+      aria-hidden="true"
+      width={22}
+      height={40}
+    />
+  );
+}
+
+export default Spring;
