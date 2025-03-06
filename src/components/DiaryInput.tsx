a9197d66097f9181f5ccc2b55a846f51aeb1c0ea
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-03-06 09:44:17 +0900

    [Refactor] Diary컴포넌트
    - type값에 따른 input, textarea 조건부 렌더링
    - placeholder 제거 및 label로 대체
    - input, textarea를 label 과 연결
    - tm함수 적용
    - input, textarea에 값 입력 시 label값 숨김 처리

--- src/components/DiaryInput.tsx
+++ src/components/DiaryInput.tsx
@@ -1,34 +1,47 @@
-import '../styles/global.css';
+import { tm } from '@/utils/tw-merge';
+import { useId } from 'react';
 
-interface DiaryInput {
+interface DiaryInpuProps {
   text: string;
-  placeholder: string;
-  type: "input" | "textArea";
-  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+  label: string;
+  type: 'input' | 'textarea';
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
 }
 
-function DiaryInput({text,type,placeholder,onChange}:DiaryInput) {
+function DiaryInput({ text, label, type, onChange }: DiaryInpuProps) {
+  const fieldId = useId();
+  const hideLabel = text.length > 0;
 
   return (
-    <li className='list-none w-full'>
-      {type === "textArea" ?(
-        <textarea className='bg-transparent border border-solid border-[var(--logo-green)]' 
-        value={text}
-        onChange={onChange}
-        placeholder={placeholder}
+    <li className="list-none w-full p-2">
+      <label
+        htmlFor={fieldId}
+        className={tm(
+          'text-[var(--light-gray)] transition-opacity duration-100 absolute w-full min-h-52',
+          hideLabel ? 'opacity-0' : 'opacity-100'
+        )}
+      >
+        {label}
+      </label>
+      {type === 'textarea' ? (
+        <textarea
+          id={fieldId}
+          className="bg-transparent border rounded-sm border-solid border-[var(--logo-green)] resize-none"
+          value={text}
+          onChange={onChange}
         />
       ) : (
-        <input className='bg-transparent w-full border-b border-solid border-[var(--logo-green)] '
-        value={text}
-        onChange={onChange}
-        placeholder={placeholder}
+        <input
+          id={fieldId}
+          className="bg-transparent w-full border-b border-solid border-[var(--logo-green)] "
+          value={text}
+          onChange={onChange}
         />
       )}
     </li>
-  )
-
+  );
 }
 
-
-
-export default DiaryInput
+export default DiaryInput;
