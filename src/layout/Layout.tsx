738555c30ae3b1582a11a21a4a2706973e182bb5
Min-gyu <jorushs@gmail.com>
2025-03-11 14:33:12 +0900

    [Modfiy] 네비게이션바 컴포넌트 경로 변경 및 레이아웃 컴포넌트에 삽입

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,25 @@
+import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import { ReactNode } from 'react';
+import NavigationBar from './NavigationBar';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === '/sign-up' || location.pathname === '/sign-in';
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header></Header>
+      <Header />
       <main className="flex-1 flex flex-col">{children}</main>
-      <Footer></Footer>
+      {isAuthPage ? <Footer /> : <NavigationBar />}
     </div>
   );
 }
+
 export default Layout;
