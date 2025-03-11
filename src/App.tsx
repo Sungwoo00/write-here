fe2b7bdd8cc66fe2d7bac97805ca61b214a00fcf
Min-gyu <jorushs@gmail.com>
2025-03-11 14:32:38 +0900

    [Modify] 라우터 연결 - 페이지 컴포넌트 전부 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import Landing from './pages/landing';
 import NotFound from './pages/not-found';
 import SignIn from './pages/sign-in';
 import SignUp from './pages/sign-up';
+import Diary from './pages/diary';
+import DiaryDetail from './pages/diary-detail';
+import DiaryRegister from './pages/diary-register';
+import Profile from './pages/profile';
+import PublicDiary from './pages/public-diary';
+import WriteHereMaP from './pages/write-here-map';
+import DiaryCalendar from './pages/calendar';
 
 function App() {
   return (
@@ -13,6 +20,13 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary-detail" element={<DiaryDetail />} />
+          <Route path="/diary-register" element={<DiaryRegister />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/public-diary" element={<PublicDiary />} />
+          <Route path="/write-here-map" element={<WriteHereMaP />} />
+          <Route path="/diary-calendar" element={<DiaryCalendar />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
