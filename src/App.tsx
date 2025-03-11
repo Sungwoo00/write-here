9306a978a029f9441d5170c9602482d1fc97ed39
Min-gyu <jorushs@gmail.com>
2025-03-11 11:04:35 +0900

    [Feat] SignUp 추가 - 반응형 구현 - validation 디바운스 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import Layout from './layout/Layout';
 import Landing from './pages/landing';
 import NotFound from './pages/not-found';
 import SignIn from './pages/sign-in';
-import LikeCounter from './components/level-2/LikeCounter';
+import SignUp from './pages/sign-up';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <LikeCounter />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
