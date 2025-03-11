7a4a12a984623b85586b5ff68adc97a6ef6856e2
Min-gyu <jorushs@gmail.com>
2025-03-11 11:02:41 +0900

    [Design] 레이아웃 컴포넌트 수정 - main 영역 css 수정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -10,7 +10,7 @@ function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header></Header>
-      <main className="flex-1 w-full max-w-screen-xl mx-auto">{children}</main>
+      <main className="flex-1 flex flex-col">{children}</main>
       <Footer></Footer>
     </div>
   );
