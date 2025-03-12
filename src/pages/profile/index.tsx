f95abd64472bfb333d86a1cb6e2e54cfd47079dd
Min-gyu <jorushs@gmail.com>
2025-03-12 19:23:19 +0900

    [Feat] 로그아웃, 회원탈퇴 임시 구현 - profile 페이지에 임시로 구현

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,75 @@
+import Modal from '@/components/level-2/Modal';
+import supabase from '@/utils/supabase';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function Profile() {
-  return <div className="flex-grow"></div>;
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMsg, setModalMsg] = useState('');
+  const [onConfirm, setOnConfirm] = useState<() => Promise<void>>(
+    () => async () => {}
+  );
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error('로그아웃 실패:', error.message);
+      return;
+    }
+    navigate('/');
+  };
+
+  const deleteUser = async () => {
+    const { error } = await supabase.rpc('delete_current_user');
+    if (error) {
+      console.error('회원탈퇴 실패:', error.message);
+      return;
+    }
+    navigate('/good-bye');
+  };
+
+  const handleSignOutClick = () => {
+    setModalMsg('로그아웃 하시겠습니까?');
+    setOnConfirm(() => signOut);
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteUserClick = () => {
+    setModalMsg('정말로 회원탈퇴 하시겠습니까?');
+    setOnConfirm(() => deleteUser);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <div className="flex-grow flex flex-col font-[HSSanTokki]">
+      <button
+        type="button"
+        className="text-[var(--icon-red)] hover:underline cursor-pointer"
+        onClick={handleSignOutClick}
+      >
+        로그아웃
+      </button>
+      <button
+        type="button"
+        className="text-[var(--icon-red)] hover:underline cursor-pointer"
+        onClick={handleDeleteUserClick}
+      >
+        회원탈퇴
+      </button>
+      <Modal
+        isOpen={isModalOpen}
+        buttonConfirmText="확인"
+        buttonCancelText="취소"
+        onConfirm={onConfirm}
+        onClose={() => setIsModalOpen(false)}
+      >
+        <p className="min-h-15 mt-2 text-[var(--dark-gray)] text-center">
+          {modalMsg}
+        </p>
+      </Modal>
+    </div>
+  );
 }
 
 export default Profile;
