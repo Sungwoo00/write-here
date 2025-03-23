4379d821c19b723234602848530c755ae2c710c7
Min-gyu <jorushs@gmail.com>
2025-03-23 15:37:48 +0900

    [Design] 헤더, 레이아웃 수정 - 헤더가 고정되도록 변경 - 헤더값 고정에 따른 패딩 추가 및 배경색 제어 로직 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import NavigationBar from './NavigationBar';
+import { tm } from '@/utils/tw-merge';
 
 interface LayoutProps {
   children: ReactNode;
@@ -16,8 +17,16 @@ function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex flex-col">{children}</main>
-      {isNotNavPage ? <Footer /> : <NavigationBar />}
+      <main
+        className={tm(
+          'flex-1 flex flex-col py-16 lg:pb-0',
+          isNotNavPage ? 'bg-[var(--light-beige)]' : ''
+        )}
+      >
+        {children}
+        {!isNotNavPage && <NavigationBar />}
+      </main>
+      {isNotNavPage && <Footer />}
     </div>
   );
 }
