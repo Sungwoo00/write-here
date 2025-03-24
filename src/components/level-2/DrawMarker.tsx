3eb06d48d87ea14085d8cf9d20e006b5d126491e
Min-gyu <jorushs@gmail.com>
2025-03-25 07:18:43 +0900

    [Modify] DrawMarker 컴포넌트 수정
    - 검색 결과로 나온 마커를 처리하도록 분기

--- src/components/level-2/DrawMarker.tsx
+++ src/components/level-2/DrawMarker.tsx
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
 import { Marker } from '@/store/DiaryData';
-import { useMapStore } from '@/store/Map';
+import { SearchLocationInfo, useMapStore } from '@/store/Map';
 
 interface RenderMarkerProps {
   map: kakao.maps.Map | null;
   marker: Marker;
+  searchLocationInfo?: SearchLocationInfo;
 }
 
-function DrawMarker({ map, marker }: RenderMarkerProps) {
-  const { isOverlayOpen, openOverlay } = useMapStore();
+function DrawMarker({ map, marker, searchLocationInfo }: RenderMarkerProps) {
+  const {
+    isOverlayOpen,
+    openOverlay,
+    setSearchLocationInfo,
+    setTempMarkerLocation,
+  } = useMapStore();
   useEffect(() => {
     if (!map) return;
     const { lat, lon, marker_path, marker_id } = marker;
@@ -17,13 +23,16 @@ function DrawMarker({ map, marker }: RenderMarkerProps) {
     const position = new kakao.maps.LatLng(lat, lon);
     const imageSize = new kakao.maps.Size(40, 40);
     const markerImage = new kakao.maps.MarkerImage(marker_path, imageSize);
-
     const newMarker = new kakao.maps.Marker({
       position,
       image: markerImage,
       map,
     });
     const handleMarkerClick = () => {
+      if (searchLocationInfo) {
+        setSearchLocationInfo(searchLocationInfo);
+        setTempMarkerLocation(lat, lon);
+      }
       if (!isOverlayOpen) {
         map.setCenter(position);
         map.panBy(0, 300);
@@ -34,11 +43,18 @@ function DrawMarker({ map, marker }: RenderMarkerProps) {
     };
 
     kakao.maps.event.addListener(newMarker, 'click', handleMarkerClick);
-
     return () => {
       newMarker.setMap(null);
     };
-  }, [isOverlayOpen, openOverlay, map, marker]);
+  }, [
+    isOverlayOpen,
+    openOverlay,
+    map,
+    marker,
+    searchLocationInfo,
+    setSearchLocationInfo,
+    setTempMarkerLocation,
+  ]);
 
   return null;
 }
